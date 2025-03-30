70e41c8c817d312328cace3f6ced2d438bd018c5
BuriyMedved <test@example.com>
2025-03-30 17:02:48 +0300

    new: контроллер в реактивном стиле

--- modules/api-gateway/src/main/java/org/buriy/medved/backend/rest/ArticlesController.kt
+++ modules/api-gateway/src/main/java/org/buriy/medved/backend/rest/ArticlesController.kt
@@ -1,13 +1,14 @@
 package org.buriy.medved.backend.rest
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.MediaType
+import org.springframework.scheduling.annotation.Async
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 
 @RestController
@@ -15,24 +16,23 @@ class ArticlesController(private val webClient: WebClient) {
     companion object {
         val logger = LoggerFactory.getLogger(ArticlesController::class.java)
     }
+
+    @Async
     @GetMapping(value = ["/api/articles"])
     fun getArticlesApi(
         @RegisteredOAuth2AuthorizedClient("articles-client-authorization-code") authorizedClient: OAuth2AuthorizedClient?
-    ): Array<String> {
+    ): Mono<Array<String>> {
         try {
-            val result: Array<String>?
-            result = webClient
+            val bodyToMono = webClient
                 .get()
                 .uri("http://localhost:9080/api/v1/articles/list")
                 .attributes(oauth2AuthorizedClient(authorizedClient))
                 .retrieve()
                 .bodyToMono(Array<String>::class.java)
-                .block()
-
-            return result
+            return bodyToMono
         } catch (e: Exception) {
             logger.error("", e)
-            return emptyArray()
+            return Mono.just(emptyArray())
         }
     }
 
@@ -57,10 +57,5 @@ class ArticlesController(private val webClient: WebClient) {
 //            logger.error("", e)
 //            return ""
 //        }
-//    }
-
-//    @PostMapping(value = ["/authorized"])
-//    fun authorized(){
-//        println("!!!!!authorized")
 //    }
 }
